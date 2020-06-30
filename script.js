6fa69d892d60ba26a239cd1793f9a5fe9dbd0115
Oleg <gentcomeone@gmail.com>
2020-06-30 18:18:31 +0300

    Task script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log(5%2);
+
+const numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?',  '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", '');
+const b = prompt('На сколько оцените его?', '');
+const c = prompt("Один из последних просмотренных фильмов?", "");
+const d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
